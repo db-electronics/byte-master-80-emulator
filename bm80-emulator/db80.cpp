9bb723ecd9887036aca9477805e008d59b7a7256
René Richard <reneleonrichard@gmail.com>
2024-01-30 11:54:38 -0500

    basic instruction structure is down

--- bm80-emulator/db80.cpp
+++ bm80-emulator/db80.cpp
@@ -3,12 +3,193 @@
 
 db80::db80()
 {
+	reset();
+	using c = db80;
+	instructions =
+	{
+		{ "nop", 0x00, &c::imp, &c::nop, 4 }, { "ld bc,nn", 0x01, &c::imx, &c::ldrp, 10 }, { "ld (bc),a", 0x02, &c::indreg, &c::ldmb, 7 }, { "inc bc", 0x03, &c::imp, &c::incrp, 6 }
+	};
 }
 
 db80::~db80()
 {
+
 }
 
+void db80::reset(void) {
+	afp = 0;
+	rpp = 0;
+
+	sp = 0x0000;
+	pc = 0x0000;
+}
+
+
+
+// addressing modes
+void db80::imp(uint8_t opcode) {
+
+}
+
+void db80::reg(uint8_t r) {
+	switch (r) {
+		case 0: 
+			alu_temp = bc[rpp].b; break;
+		case 1: 
+			alu_temp = bc[rpp].c; break;
+		case 2: 
+			alu_temp = de[rpp].d; break;
+		case 3: 
+			alu_temp = de[rpp].e; break;
+		case 4: 
+			alu_temp = hl[rpp].h; break;
+		case 5: 
+			alu_temp = hl[rpp].l; break;
+		case 7: 
+			alu_temp = af[afp].a; break;
+		default: break;
+	}
+}
+
+void db80::rgx(uint8_t s) {
+	switch (s) {
+	case 0: 
+		op16 = bc[rpp].word; break;
+	case 1: 
+		op16 = de[rpp].word; break;
+	case 2: 
+		op16 = hl[rpp].word; break;
+	case 3: 
+		op16 = sp; break;
+	}
+}
+
+void db80::imm(uint8_t opcode) {
+	op8 = memoryRead(pc++);
+}
+
+void db80::imx(uint8_t opcode) {
+	op16 = (memoryRead(pc++) | memoryRead(pc++) << 8);
+}
+
+void db80::abs(uint8_t opcode) {
+	addr_abs = (memoryRead(pc++) | memoryRead(pc++) << 8);
+}
+
+void db80::indreg(uint8_t opcode) {
+	switch (opcode) {
+	case 0x02:
+		addr_abs = bc[rpp].word;
+		op8 = af[afp].a;
+		break;
+	case 0x12:
+		addr_abs = de[rpp].word;
+		op8 = af[afp].a;
+		break;
+	case 0x22:
+		addr_abs = (memoryRead(pc++) | memoryRead(pc++) << 8);
+		op16 = hl[rpp].word;
+		break;
+	case 0x32:
+		addr_abs = (memoryRead(pc++) | memoryRead(pc++) << 8);
+		op8 = af[afp].a;
+		break;
+	}
+}
+
+void db80::zpg(uint8_t opcode) {
+	// instruction is 11ppp111 where ppp is the lower byte of the rst address call
+	addr_abs = (uint16_t)(opcode & 0x38);
+}
+
+
+// instructions
+uint8_t db80::nop(uint8_t op){
+	return 0;
+}
+
+uint8_t db80::incrp(uint8_t op) {
+	switch (op) {
+	case 0x03:
+		bc[rpp].word++; break;
+	case 0x13:
+		de[rpp].word++; break;
+	case 0x23:
+		hl[rpp].word++; break;
+	case 0x33:
+		sp++; break;
+	}
+	return 0;
+}
+
+uint8_t db80::ldmb(uint8_t op) {
+	memoryWrite(addr_abs, op8);
+	return 0;
+}
+
+uint8_t db80::ldr(uint8_t op) {
+	// 00rrr110 - Load register rrr with 8 bit immediate
+	if ((op & 0b11000111) == 0b00000110) {
+		switch ((op >> 3) & 0x07) {
+		case 0:
+			bc[rpp].b = op8; break;
+		case 1:
+			bc[rpp].c = op8; break;
+		case 2:
+			de[rpp].d = op8; break;
+		case 3:
+			de[rpp].e = op8; break;
+		case 4:
+			hl[rpp].h = op8; break;
+		case 5:
+			hl[rpp].l = op8; break;
+		case 7:
+			af[afp].a = op8; break;
+		default: break;
+		}
+	}
+	// 01dddrrr load register ddd with register rrr
+	else if ((op & 0b11000000) == 0b01000000) {
+		switch ((op >> 3) & 0x07) {
+		case 0:
+			bc[rpp].b = op8; break;
+		case 1:
+			bc[rpp].c = op8; break;
+		case 2:
+			de[rpp].d = op8; break;
+		case 3:
+			de[rpp].e = op8; break;
+		case 4:
+			hl[rpp].h = op8; break;
+		case 5:
+			hl[rpp].l = op8; break;
+		case 7:
+			af[afp].a = op8; break;
+		default: break;
+		}
+	}
+	return 0;
+}
+
+uint8_t db80::ldrp(uint8_t op) {
+	// 0x01 bc
+	// 0x11 de
+	// 0x21 hl
+	// 0x31 sp
+	switch (op) {
+	case 0x01:
+		bc[rpp].word = op16; break;
+	case 0x11:
+		de[rpp].word = op16; break;
+	case 0x21:
+		hl[rpp].word = op16; break;
+	case 0x31:
+		sp = op16; break;
+	default: break;
+	}
+}
+
+// bus operations
 uint8_t db80::memoryRead(uint16_t address)
 {
 	return bus->memoryRead(address);
--- bm80-emulator/db80.h
+++ bm80-emulator/db80.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
+#include <vector>
 
 // forward declare system
 class ByteMaster80;
@@ -15,25 +17,49 @@ public:
 		C  = (1 << 0), // carry
 		N  = (1 << 1), // substract
 		PV = (1 << 2), // parity/overflow
-		U3 = (1 << 3), // unused
 		H  = (1 << 4), // half-carry
-		U5 = (1 << 5), // unused
 		Z  = (1 << 6), // zero
 		S  = (1 << 7)  // sign
 	};
 
+	void reset();
+	void connectBus(ByteMaster80* n) { bus = n; }
+
 	// addressing modes
-	void imp(uint8_t opcode); // implied
-	void imm(uint8_t opcode); // immediate
-	void imx(uint8_t opcode); // immediate extended
-	void abs(uint8_t opcode); // absolute
+	void imp(uint8_t); // implied
+	void reg(uint8_t r); // register 8 bit source
+	void rgx(uint8_t s); // register 16 bit source
+	void imm(uint8_t); // immediate
+	void imx(uint8_t); // immediate extended
+	void abs(uint8_t); // absolute
+	void indreg(uint8_t opcode); // rp indirect destination, register source
 	void zpg(uint8_t opcode); // zero page reset
 	void rel(uint8_t opcode); // relative
 	void inx(uint8_t opcode); // indexed x
 	void iny(uint8_t opcode); // indexed y
-	void ind(uint8_t opcode); // indirect
+	
+	// instruction mnemonics
+	struct _instructions
+	{
+		std::string name;									/**< opcode name */
+		uint8_t     opcode;									/**< opcode byte */
+		void		(db80::* addrmode)(uint8_t) = nullptr;
+		uint8_t		(db80::* operate)(uint8_t) = nullptr;	/**< function pointer implementing the opcode */
+		uint8_t     cycles = 0;								/**< required cycles for operation, lowest amount */
+	};
 
-	void connectBus(ByteMaster80* n) { bus = n; }
+	std::vector<_instructions> instructions;
+
+	uint8_t nop(uint8_t);
+
+	// INC
+	uint8_t incrp(uint8_t);	// increment register pair
+
+	// LOAD
+	uint8_t ldmb(uint8_t);	// load memory byte
+	uint8_t ldr(uint8_t);	// load register
+	uint8_t ldrp(uint8_t);	// load register pair
+	
 
 private:
 	ByteMaster80* bus = nullptr;
@@ -42,5 +68,92 @@ private:
 	void memoryWrite(uint16_t address, uint8_t data);
 	uint8_t ioRead(uint8_t address);
 	void ioWrite(uint8_t address, uint8_t data);
+
+
+	// thanks to https://github.com/redcode/Z80/blob/master/sources/Z80.c for parity lookup
+	uint8_t parityLookup[256] = {
+		/*	    0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F */
+		/* 0 */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* 1 */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* 2 */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* 3 */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* 4 */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* 5 */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* 6 */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* 7 */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* 8 */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* 9 */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* A */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* B */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* C */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4,
+		/* D */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* E */ 0, 4, 4, 0, 4, 0, 0, 4, 4, 0, 0, 4, 0, 4, 4, 0,
+		/* F */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4
+	};
+
+	// EXX register re-naming
+
+	uint8_t alu_temp;
+	uint8_t op8;
+	uint16_t op16;
+	uint16_t addr_abs;
+	uint16_t addr_rel;
+
+	uint8_t afp;	// rename a and f registers
+	uint8_t rpp;	// rename bc, de and hl registers
+
+	struct _AF {
+		uint8_t a;
+		union _flags {
+			struct {
+				unsigned C : 1;
+				unsigned N : 1;
+				unsigned PV : 1;
+				unsigned : 1;
+				unsigned H : 1;
+				unsigned : 1;
+				unsigned Z : 1;
+				unsigned S : 1;
+			};
+			uint8_t byte;
+		}f;
+	};
+	_AF af[2];
+
+	// BC register
+	union _BC {
+		struct {
+			uint8_t b;
+			uint8_t c;
+		};
+		uint16_t word;
+	};
+
+	_BC bc[2];
+
+	// DE register
+	union _DE {
+		struct {
+			uint8_t d;
+			uint8_t e;
+		};
+		uint16_t word;
+	};
+
+	_DE de[2];
+
+	// HL register
+	union _HL {
+		struct {
+			uint8_t h;
+			uint8_t l;
+		};
+		uint16_t word;
+	};
+
+	_HL hl[2];
+
+	uint8_t i, r, iff2;
+	uint16_t sp, pc, ix, iy;
 };
 
