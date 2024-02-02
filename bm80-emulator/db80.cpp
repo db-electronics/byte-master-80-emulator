73d8631ce43d39142330f8c047fc5e4caf6168f6
René Richard <reneleonrichard@gmail.com>
2024-02-01 21:18:46 -0500

    cycle approach, much more intuitive

--- bm80-emulator/db80.cpp
+++ bm80-emulator/db80.cpp
@@ -1,281 +1,163 @@
 #include "db80.h"
 #include "ByteMaster80.h"
 
-db80::db80()
-{
-	reset();
-	using c = db80;
-	instructions =
-	{
-		{ "nop",       0x00, 4,  &c::imp, &c::nop }, 
-		{ "ld bc,nn",  0x01, 10, &c::imxrpd, &c::ldrp },
-		{ "ld (bc),a", 0x02, 7,  &c::indrpa, &c::ldm },
-		{ "inc bc",    0x03, 6,  &c::regp, &c::incrp },
-		{ "inc b",     0x04, 4,  &c::reg, &c::incr }, 
-		{ "dec b",     0x05, 4,  &c::reg, &c::decr }, 
-		{ "ld b,n",    0x06, 7,  &c::immrd, &c::ldr },
-		{ "rlca",      0x07, 4,  &c::imp, &c::rlca },
-		{ "ex af,af`", 0x08, 4,  &c::imp, &c::exaf },
-		{ "add hl,bc", 0x09, 11, &c::hldrp, &c::addrp }, // 587, 203
-		{ "ld a,(bc)", 0x0A, 7,  &c::indrp, &c::ldr }, // 614, 329
-		{ "dec bc",    0x0B, 6,  &c::regp, &c::decrp },
-	};
+
+db80::db80() {
+	CtrlPins = 0;
+	AddrPins = 0;
+	DataPins = 0;
+	z80.state = Z_RESET;
+
+	z80.r = 0;
+	z80.ir = 0;
+	z80.tState = 0;
 }
 
-db80::~db80()
-{
-
-}
-
-void db80::reset(void) {
-
-	sp = 0x0000;
-	pc = 0x0000;
-	currentOpCycles = 0;
-}
-
-void db80::setFlag(uint8_t f, bool condition) {
-	if (condition) {
-		af.flags |= f;
-	}
-	else {
-		af.flags &= ~f;
-	}
-}
-
-uint32_t db80::clock(int32_t runForCycles) {
-	static _instruction instr;
-
-	while (runForCycles > 0) {
-		if (currentOpCycles == 0) {
-			instr = instructions[memoryRead(pc++)];
-			r = (r + 1) & 0x7F;
-
-			// addressing mode
-			currentOpCycles = instr.cycles;
-			(this->*instr.addrmode)(instr.opcode);
-			// some instructions can add cycles
-			currentOpCycles += (this->*instr.operate)(instr.opcode);
+uint32_t db80::tick(uint32_t cycles) {
+	z80.tState++;
+	switch (z80.state) {
+	case Z_RESET:
+		CtrlPins = 0;
+		if (z80.tState > 4) {
+			z80.state = Z_OPCODE_FETCH;
+			z80.tState = 1;
 		}
-		currentOpCycles--;
-		runForCycles--;
-	}
-	// TODO
-	// return difference in cycles
-	return currentOpCycles;
-}
-
-
-// addressing modes
-// implied addressing, nothing to do
-void db80::imp(uint8_t opcode) {
-
-}
-
-// register is destination
-void db80::reg(uint8_t opcode) {
-	reg8 = getRegister((opcode >> 3) & 0x07);
-}
-
-// register pair is destination
-void db80::regp(uint8_t opcode) {
-	reg16 = getRegisterPair((opcode >> 4) & 0x03);
-}
-
-void db80::hldrp(uint8_t opcode) {
-	reg16 = getRegisterPair(HL_SEL);
-	op16 = getRegisterPairValue((opcode >> 4) & 0x03);
-}
-
-void db80::imm(uint8_t opcode) {
-	op8 = memoryRead(pc++);
-}
-
-void db80::immrd(uint8_t opcode) {
-	op8 = memoryRead(pc++);
-	reg8 = getRegister((opcode >> 3) & 0x07);
-}
-
-void db80::imx(uint8_t opcode) {
-	op16 = (memoryRead(pc++) | memoryRead(pc++) << 8);
-}
-
-void db80::imxrpd(uint8_t opcode) {
-	op16 = (memoryRead(pc++) | memoryRead(pc++) << 8);
-	reg16 = getRegisterPair((opcode >> 4) & 0x03);
-}
-
-void db80::indrpa(uint8_t opcode) {
-	op8 = af.acc;
-	reg16 = getRegisterPair((opcode >> 4) & 0x03);
-	addr_abs = *reg16;
-}
-
-void db80::indrp(uint8_t opcode) {
-	reg8 = getRegister(ACC_SEL);
-	addr_abs = getRegisterPairValue((opcode >> 4) & 0x03);
-	op8 = memoryRead(addr_abs);
-}
-
-void db80::abs(uint8_t opcode) {
-	addr_abs = (memoryRead(pc++) | memoryRead(pc++) << 8);
-}
-
-void db80::zpg(uint8_t opcode) {
-	// instruction is 11ppp111 where ppp is the lower byte of the rst address call
-	addr_abs = (uint16_t)(opcode & 0x38);
-}
-
-///////////////////////////////////////////////////////
-// instructions
-uint8_t db80::nop(uint8_t op){
-	return 0;
-}
-
-// p 587 Ramesh Goankar
-// p 203 Rodnay Zaks
-uint8_t db80::addrp(uint8_t op) {
-	*reg16 += op16;
-	setFlag(N, 0);
-	// TODO verify this Carry flag
-	setFlag(C, (*reg16 & 0x8000) != (op16 & 0x8000));
-	setFlag(H, (*reg16 & 0x0800) != (op16 & 0x0800));
-	return 0;
-}
-
-uint8_t db80::decr(uint8_t op) {
-	// 00rrr101 - decrement register
-	// TODO status flags
-	(*reg8)--;
-	setFlag(Z, (*reg8 == 0));
-	setFlag(PV, (*reg8 == 0xFF));
-	setFlag(S, (*reg8 & 0x80));
-	return 0;
-}
-
-uint8_t db80::decrp(uint8_t op) {
-	(*reg16)--;
-	return 0;
-}
-
-uint8_t db80::exaf(uint8_t op) {
-	op16 = afp.pair;
-	afp.pair = af.pair;
-	af.pair = op16;
-	return 0;
-}
-
-
-uint8_t db80::incr(uint8_t op) {
-	// 00rrr100 - increment register
-	// TODO status flags
-	(*reg8)++;
-	setFlag(Z | PV, (*reg8 == 0));
-	setFlag(S, (*reg8 & 0x80));
-	return 0;
-}
-
-uint8_t db80::incrp(uint8_t op) {
-	// 00rr0011 - increment register pair rr
-	(reg16)++;
-	return 0;
-}
-
-uint8_t db80::ldm(uint8_t op) {
-	memoryWrite(addr_abs, op8);
-	return 0;
-}
-
-uint8_t db80::ldr(uint8_t op) {
-	// 00rrr110 - Load register rrr with 8 bit immediate
-	// 01dddrrr load register ddd with register rrr
-	*reg8 = op8;
-	return 0;
-}
-
-uint8_t db80::ldrp(uint8_t op) {
-	// 00rr0001 - load register pair rr
-	*reg16 = op16;
-	return 0;
-}
-
-uint8_t db80::rlca(uint8_t op) {
-	uint8_t carry = (af.acc & 0x80) ? 1 : 0;
-	setFlag(C, carry);
-	setFlag(H | N, 0);
-	af.acc = (af.acc << 1) | carry;
-	return 0;
-}
-
-///////////////////////////////////////////////////////
-// bus operations
-uint8_t db80::memoryRead(uint16_t address)
-{
-	return bus->memoryRead(address);
-}
-
-void db80::memoryWrite(uint16_t address, uint8_t data)
-{
-	bus->memoryWrite(address, data);
-}
-
-uint8_t db80::ioRead(uint8_t address)
-{
-	return bus->ioRead(address);
-}
-
-void db80::ioWrite(uint8_t address, uint8_t data)
-{
-	bus->ioWrite(address, data);
-}
-
-uint8_t* db80::getRegister(uint8_t r) {
-	switch (r) {
-	case 0:
-		return &bc.b;
-	case 1:
-		return &bc.c;
-	case 2:
-		return &de.d;
-	case 3:
-		return &de.e;
-	case 4:
-		return &hl.h;
-	case 5:
-		return &hl.l;
-	case 7:
-		return &af.acc;
-	default: break;
+		break;
+	case Z_OPCODE_FETCH:
+		switch (z80.tState) {
+		case 1:
+			CtrlPins = (MREQ | RD | M1);
+			AddrPins = z80.pc++;
+			break;
+		case 2:
+			z80.ir = DataPins;
+			z80.r++;
+			break;
+		case 3:
+			// totally ignoring refresh
+			CtrlPins &= ~(MREQ | RD | M1);
+			break;
+		case 4:
+			opFetch();
+			break;
+		}
+		break;
+	case Z_M1_EXT:
+		switch (z80.tState) {
+		case 2:
+			z80.tState = 0;
+			z80.state = Z_OPCODE_FETCH;
+			break;
+		default:
+			break;
+		}
+		break;
+	case Z_MEMORY_READ:
+		switch (z80.tState) {
+		case 1:
+			AddrPins = z80.pc++;
+			break;
+		case 2:
+			CtrlPins |= (MREQ | RD);
+			*z80.regDest = DataPins;
+			break;
+		case 3:
+			CtrlPins &= ~(MREQ | RD );
+			z80.tState = 0;
+			opMemRead();
+			break;
+		}
+		break;
+	case Z_MEMORY_READ_EXT:
+		switch (z80.tState) {
+		case 1:
+			AddrPins = z80.pc++;
+			break;
+		case 2:
+			CtrlPins |= (MREQ | RD);
+			z80.wz.z = DataPins;
+			break;
+		case 3:
+			CtrlPins &= ~(MREQ | RD);
+			break;
+		case 4:
+			AddrPins = z80.pc++;
+			break;
+		case 5:
+			CtrlPins |= (MREQ | RD);
+			z80.wz.w = DataPins;
+			break;
+		case 6:
+			CtrlPins &= ~(MREQ | RD);
+			*z80.regPairDest = z80.wz.pair;
+			z80.tState = 0;
+			break;
+		}
+		break;
+	case Z_MEMORY_WRITE:
+		switch (z80.tState) {
+		case 1:
+			AddrPins = z80.addr;
+			break;
+		case 2:
+			CtrlPins |= (MREQ | WR);
+			DataPins = z80.data;
+			break;
+		case 3:
+			CtrlPins &= ~(MREQ | WR);
+			z80.tState = 0;
+			z80.state = Z_OPCODE_FETCH;
+			break;
+		}
+		break;
+	default:
+		break;
 	}
 	return 0;
 }
 
-uint16_t* db80::getRegisterPair(uint8_t rp) {
-	switch (rp) {
-	case 0:
-		return &bc.pair;
-	case 1:
-		return &de.pair;
-	case 2:
-		return &hl.pair;
-	case 3:
-		return &sp;
-	default: break;
+void db80::opFetch(void) {
+	switch (z80.ir) {
+	case 0x00: // NOP
+		z80.state = Z_OPCODE_FETCH;
+		return;
+	case 0x01: // LD BC,nn
+		z80.regPairDest = &z80.bc.pair;
+		z80.state = Z_MEMORY_READ_EXT;
+		return;
+	case 0x02: // LD (BC),a
+		z80.data = z80.af.acc;
+		z80.addr = z80.bc.pair;
+		z80.state = Z_MEMORY_WRITE;
+		return;
+	case 0x03: // INC BC
+		z80.bc.pair++;	// I know this only happens later, but meh this is still externally cycle accurate
+		z80.state = Z_M1_EXT;
+		return;
+	case 0x04: // INC B
+		incReg(z80.bc.b);
+		z80.state = Z_OPCODE_FETCH;
+		return;
+	case 0x05: // DEC B
+		decReg(z80.bc.b);
+		z80.state = Z_OPCODE_FETCH;
+		return;
+	case 0x06: // LD B,n
+		z80.regDest = &z80.bc.b;
+		z80.state = Z_MEMORY_READ;
+		return;
+	default:
+		z80.state = Z_OPCODE_FETCH;
+		return;
 	}
-	return 0;
 }
 
-uint16_t db80::getRegisterPairValue(uint8_t rp) {
-	switch (rp) {
-	case 0:
-		return bc.pair;
-	case 1:
-		return de.pair;
-	case 2:
-		return hl.pair;
-	case 3:
-		return sp;
-	default: break;
-	}
-	return 0;
+inline void db80::decReg(uint8_t& reg) {
+	reg--;
+	// TODO SET FLAGS
+}
+
+inline void db80::incReg(uint8_t& reg) {
+	reg++;
+	// TODO SET FLAGS
 }
--- bm80-emulator/db80.h
+++ bm80-emulator/db80.h
@@ -13,79 +13,101 @@ public:
 	db80();
 	~db80();
 
-	enum flags {
-		C  = (1 << 0), // carry
-		N  = (1 << 1), // substract
-		PV = (1 << 2), // parity/overflow
-		H  = (1 << 4), // half-carry
-		Z  = (1 << 6), // zero
-		S  = (1 << 7)  // sign
+	// Exposed Pins
+	uint16_t CtrlPins;
+	uint16_t AddrPins;
+	uint16_t DataPins;
+
+	enum PINS {
+		MREQ = (1 << 0),
+		IORQ = (1 << 1),
+		RD   = (1 << 2),
+		WR   = (1 << 3),
+		M1   = (1 << 4),
+		WAIT = (1 << 5)
 	};
 
-	//const uint8_t C_flag = (1 << 0);
-	//const uint8_t N_flag = (1 << 1);
-	//const uint8_t PV_flag = (1 << 2);
-	//const uint8_t H_flag = (1 << 4);
-	//const uint8_t Z_flag = (1 << 6);
-	//const uint8_t S_flag = (1 << 7);
-
-	void reset();
-	uint32_t clock(int32_t runForCycles);
+	uint32_t tick(uint32_t cycles);
 	void connectBus(ByteMaster80* n) { bus = n; }
-	void setFlag(uint8_t f, bool condition);
 
-	// addressing modes, sssddd (source, destination)
-	void imp(uint8_t); // implied
-	void reg(uint8_t); // register destination --rrr---
-	void indrp(uint8_t); // accumulator destination, indirect register pair
-	void regp(uint8_t); // register pair destination --rr----
-	void hldrp(uint8_t); // hl destination, rp source
-	void imm(uint8_t); // immediate
-	void immrd(uint8_t); // immediate, register destination --rrr---
-	void imx(uint8_t); // immediate extended
-	void imxrpd(uint8_t); // immediate extended, register pair destination
-	void indrpa(uint8_t); // indirect register pair, accumulator source
-	void abs(uint8_t); // absolute
-	void zpg(uint8_t opcode); // zero page reset
-
-	
-	// instruction mnemonics
-	struct _instruction
-	{
-		std::string name;									/**< opcode name */
-		uint8_t     opcode = 0;									/**< opcode byte */
-		uint8_t     cycles = 0;								/**< required cycles for operation, lowest amount */
-		void		(db80::* addrmode)(uint8_t) = nullptr;
-		uint8_t		(db80::* operate)(uint8_t) = nullptr;	/**< function pointer implementing the opcode */
-	};
-
-	std::vector<_instruction> instructions;
-
-	uint8_t nop(uint8_t);
-
-	// ADD
-	uint8_t addrp(uint8_t); // add register pair
-
-	// DEC
-	uint8_t decr(uint8_t);	// decrement register
-	uint8_t decrp(uint8_t);	// decrement register
-
-	// EXCHANGE
-	uint8_t exaf(uint8_t);	// exchange af and afp
-
-	// INC
-	uint8_t incr(uint8_t);  // increment register
-	uint8_t incrp(uint8_t);	// increment register pair
-
-	// LOAD
-	uint8_t ldm(uint8_t);	// load memory
-	uint8_t ldr(uint8_t);	// load register
-	uint8_t ldrp(uint8_t);	// load register pair
-	
-	// ROTATE
-	uint8_t rlca(uint8_t);
 
 private:
+
+	void opFetch();
+	void opMemRead();
+	void opMemReadExt();
+
+	void decReg(uint8_t& reg);
+	void incReg(uint8_t& reg);
+
+	enum FLAGS {
+		C = (1 << 0), // carry
+		N = (1 << 1), // substract
+		PV = (1 << 2), // parity/overflow
+		H = (1 << 4), // half-carry
+		Z = (1 << 6), // zero
+		S = (1 << 7)  // sign
+	};
+
+	enum Z80MACHINECYCLE {
+		Z_RESET,
+		Z_OPCODE_FETCH,
+		Z_M1_EXT,
+		Z_MEMORY_READ,
+		Z_MEMORY_READ_EXT,
+		Z_MEMORY_READ_ADDR,
+		Z_MEMORY_WRITE,
+		Z_IO_READ,
+		Z_IO_WRITE,
+		Z_INT_ACK,
+		Z_NMI,
+		Z_BUS_ACK
+	};
+
+	// z80 registers
+	struct Z80REGISTERS {
+		Z80MACHINECYCLE state;
+		uint8_t* regDest;
+		uint16_t* regPairDest;
+		uint16_t sp, pc, ix, iy, addr;
+		uint8_t i, r, tmp, ir, iff, iff2, data;
+		uint8_t tState;
+		union _AF {
+			struct {
+				uint8_t acc;
+				uint8_t flags;
+			};
+			uint16_t pair;
+		}af, afp;
+		union _BC {
+			struct {
+				uint8_t b;
+				uint8_t c;
+			};
+			uint16_t pair;
+		}bc, bcp;
+		union _DE {
+			struct {
+				uint8_t d;
+				uint8_t e;
+			};
+			uint16_t pair;
+		}de, dep;
+		union _HL {
+			struct {
+				uint8_t h;
+				uint8_t l;
+			};
+			uint16_t pair;
+		}hl, hlp;
+		union _WZ {
+			struct {
+				uint8_t w;
+				uint8_t z;
+			};
+			uint16_t pair;
+		}wz;
+	}z80;
 
 	const uint8_t BC_SEL = 0;
 	const uint8_t DE_SEL = 1;
@@ -96,14 +118,6 @@ private:
 
 	ByteMaster80* bus = nullptr;
 	
-	uint8_t memoryRead(uint16_t address);
-	void memoryWrite(uint16_t address, uint8_t data);
-	uint8_t ioRead(uint8_t address);
-	void ioWrite(uint8_t address, uint8_t data);
-
-	uint8_t* getRegister(uint8_t r);
-	uint16_t* getRegisterPair(uint8_t rp);
-	uint16_t getRegisterPairValue(uint8_t rp);
 
 
 	// thanks to https://github.com/redcode/Z80/blob/master/sources/Z80.c for parity lookup
@@ -127,57 +141,5 @@ private:
 		/* F */ 4, 0, 0, 4, 0, 4, 4, 0, 0, 4, 4, 0, 4, 0, 0, 4
 	};
 
-	uint32_t currentOpCycles;
-	uint8_t op8;
-	uint16_t op16;
-	uint8_t* reg8;
-	uint16_t* reg16;
-	uint16_t addr_abs;
-	uint16_t addr_rel;
-
-	union _AF {
-		struct {
-			uint8_t acc;
-			uint8_t flags;
-		};
-		uint16_t pair;
-	};
-	_AF af, afp;
-
-	// BC register
-	union _BC {
-		struct {
-			uint8_t b;
-			uint8_t c;
-		};
-		uint16_t pair;
-	};
-
-	_BC bc, bcp;
-
-	// DE register
-	union _DE {
-		struct {
-			uint8_t d;
-			uint8_t e;
-		};
-		uint16_t pair;
-	};
-
-	_DE de, dep;
-
-	// HL register
-	union _HL {
-		struct {
-			uint8_t h;
-			uint8_t l;
-		};
-		uint16_t pair;
-	};
-
-	_HL hl, hlp;
-
-	uint8_t i, r, iff2;
-	uint16_t sp, pc, ix, iy;
 };
 
